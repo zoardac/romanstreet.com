511756e5ac7c3c4db2b1b0b6f2977c37b3bd2788
Brad McLaughlin <brad@clevercouch.com>
2026-02-02 10:31:03 -0600

    Fix Astro Vercel SSR adapter config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from '@astrojs/tailwind';
 import preact from '@astrojs/preact';
 
 export default defineConfig({
-  integrations: [tailwind(), preact()],
   output: 'server',
   adapter: vercel(),
+  integrations: [tailwind(), preact()],
 });
